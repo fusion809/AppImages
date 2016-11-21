1041f3700a1e6e97ee261420d9df45dd33feeaa6
fusion809 <brentonhorne77@gmail.com>
2016-11-22 05:50:16 +1000

    Fixing travis.sh so it uses the right docker image

--- travis.sh
+++ travis.sh
@@ -18,7 +18,7 @@ if [ -f recipes/$RECIPE/Dockerfile ] && [ -f recipes/$RECIPE/Recipe ] ; then
   # There is a Dockerfile, hence build using Docker
   mv recipes/$RECIPE/Recipe ./out/Recipe
   sed -i -e 's|sudo ||g' ./out/Recipe # For subsurface recipe
-  OS=$(cat Dockerfile | grep FROM | cut -d ' ' -f 2)
+  OS=$(cat recipes/$RECIPE/Dockerfile | grep FROM | cut -d ' ' -f 2)
   docker run -i -v ${PWD}/out:/out $OS /bin/bash -ex /out/Recipe
 elif [ -f recipes/meta/$RECIPE/$RECIPE.yml ] ; then
   # There is no Dockerfile but a YAML file for the meta Recipe

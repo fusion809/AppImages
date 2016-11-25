58c5052612e66077df2a2c4b901a03cd808c8757
fusion809 <brentonhorne77@gmail.com>
2016-11-25 13:31:40 +1000

    Adjusting build procedure for Docker builds

--- travis.sh
+++ travis.sh
@@ -16,10 +16,7 @@ mkdir -p ./out/
 
 if [ -f recipes/$RECIPE/Dockerfile ] && [ -f recipes/$RECIPE/Recipe ] ; then
   # There is a Dockerfile, hence build using Docker
-  mv recipes/$RECIPE/Recipe ./out/Recipe
-  sed -i -e 's|sudo ||g' ./out/Recipe # For subsurface recipe
-  OS=$(cat recipes/$RECIPE/Dockerfile | grep FROM | cut -d ' ' -f 2)
-  docker run -i -v ${PWD}/out:/out "$OS" /bin/bash -ex /out/Recipe
+  bash -x recipes/$RECIPE/build.sh
 elif [ -f recipes/meta/$RECIPE/$RECIPE.yml ] ; then
   # There is no Dockerfile but a YAML file for the meta Recipe
   bash -ex recipes/meta/$RECIPE/Recipe recipes/meta/$RECIPE/$RECIPE.yml
